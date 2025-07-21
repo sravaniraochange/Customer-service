78687926e4d1f39ebd0709dc705352caa0ac1b4f
sravaniraochange <youremail@example.com>
2025-07-21 11:12:16 -0500

    init commit

---
+++ java/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+
+public class Main {
+
+   // @SqsClient(topic="sqs.qulication.addresss")
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+
+    }
+
+
+}
